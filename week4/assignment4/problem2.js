c353749c4943c2ffa24aab231e3e63961c968e37
HabzulDevArc <wra5296@gmail.com>
2025-10-27 04:56:47 -0700

    changes to boostrap file

--- week4/assignment4/problem2.js
+++ week4/assignment4/problem2.js
@@ -5,19 +5,35 @@
 function countLeaves(days) {
     let total = 0;
     let leaves = 10;
-    let outputTotal = "";
+    let outputTotal = [];
 
     for (let day = 1; day <= days; day++){
         total += leaves;
+        outputTotal.push(leaves);
         leaves += 10;
-        
     }
     
-    return total;
+    return `${total} (${outputTotal.join(" + ")})`;
 }
 
-// Test Cases
+function categorizeLeafColors(leaves) {
+    let colorCount = {};
+
+    for (let i = 0; i < leaves.length; i++) {
+        let color = leaves[i];
+        colorCount[color] = (colorCount[color] || 0) + 1;
+    }
+    
+    return colorCount;
+}
+
+// countLeaves() Test Cases
 console.log(countLeaves(1));
 console.log(countLeaves(2));
 console.log(countLeaves(4));
 console.log(countLeaves(5));
+
+// categorizeLeafColors() Test Cases
+console.log(categorizeLeafColors(["red", "yellow", "red", "brown"]));
+console.log(categorizeLeafColors(["orange", "orange", "orange"]));
+console.log(categorizeLeafColors([]));
